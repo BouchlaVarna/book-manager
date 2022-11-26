a021eca49f466a029f7c4c281a4aef2297177419
Štefan Jambrich <90748499+BouchlaVarna@users.noreply.github.com>
2022-11-27 00:29:16 +0100

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbar/navbar.component";
+
+const App = () => {
+    return (
+        <div id="app-wrapper">
+            <Navbar />
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/components/navbar/navbar.component.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div id="nav-wrapper">
+            <div id="title">
+                <h1>Book.manager</h1>
+            </div>
+            <div id="search-bar">
+                <input type="text" placeholder="Search" />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
